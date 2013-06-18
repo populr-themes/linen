d9e08af269fe6781fea29e555fb954d97cde6781
Daniel Nelson <dnelson@centresource.com>
2013-06-18 12:51:39 -0500

    min-height, not height, so that while typing, it can adjust

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@
   fixColumnHeights = function() {
     $('body').removeClass('assets-resized'); // ie7
     if ($('html').css('content') == "\u2063") {
-      $('.asset').css('height', '');
+      $('.asset').css('min-height', '');
 
     } else {
       $('#pop').imagesLoaded(function() {
         $('.columnizer-row').each(function(idx, el) {
-          $(this).find('.asset').height($(this).height());
+          $(this).find('.asset').css('min-height', $(this).height());
         });
       });
       $('body').addClass('assets-resized'); // ie7
@@ -20,7 +20,7 @@
     $(window).on('resize', _.throttle(fixColumnHeights));
     $('.columnizer-row .asset').live('initialize', _.throttle(fixColumnHeights));
     $('.columnizer-row .asset').live('destroy', function() {
-      $(this).find('.asset').css('height', '');
+      $(this).find('.asset').css('min-height', '');
     });
   });
 
